d1a6020ec38dbe678399532d09998faa5fbbf950
Katherine Wu <kwu@newrelic.com>
2016-12-03 21:40:49 -0800

    Day 3: Extract file reading into method

--- day_3.rb
+++ day_3.rb
@@ -2,10 +2,16 @@ def valid_triangle?(a, b, c)
   a+b > c && b+c > a && a+c > b
 end
 
+def read_file_as_rows(filename)
+  rows = []
+  File.readlines(filename).each {|row| rows << row.split }
+  rows
+end
+
 # part A
 # valid_triangles_count = 0
-# File.readlines('day_3_input.txt').each do |line|
-#   lengths = line.split
-#   valid_triangles_count += 1 if valid_triangle?(lengths[0].to_i, lengths[1].to_i, lengths[2].to_i)
+# file_rows = read_file_as_rows('day_3_input.txt')
+# file_rows.each do |row|
+#   valid_triangles_count += 1 if valid_triangle?(row[0].to_i, row[1].to_i, row[2].to_i)
 # end
 # puts valid_triangles_count
