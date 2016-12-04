cd6443d5b8eba821ebd168645dbcdb8f2618ade8
Katherine Wu <kwu@newrelic.com>
2016-12-03 16:51:36 -0800

    Day 2: Add additional line-by-line test cases

--- day_2_tests.rb
+++ day_2_tests.rb
@@ -4,10 +4,32 @@ require './day_2'
 class Day2Test < Minitest::Test
 
   def test_determine_next_position
-    instructions = "ULL"
-    starting_position = Position.new(1,1)
-    expected_next_position = Position.new(0,0)
+    test_cases = [
+      {
+        instructions: "ULL",
+        starting_position: Position.new(1,1),
+        expected_next_position: Position.new(0,0)
+      },
+      {
+        instructions: "RRDDD",
+        starting_position: Position.new(0,0),
+        expected_next_position: Position.new(2,2)
+      },
+      {
+        instructions: "LURDL",
+        starting_position: Position.new(2,2),
+        expected_next_position: Position.new(1,2)
+      },
+      {
+        instructions: "UUUUD",
+        starting_position: Position.new(1,2),
+        expected_next_position: Position.new(1,1)
+      }
+    ]
 
-    assert_equal expected_next_position, determine_next_position(starting_position, instructions)
+    test_cases.each do |test_case|
+      result = determine_next_position(test_case[:starting_position], test_case[:instructions])
+      assert_equal test_case[:expected_next_position], result
+    end
   end
 end
