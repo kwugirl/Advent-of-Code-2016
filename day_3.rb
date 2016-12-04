89244f0929e7ed146e96f56ea0ef21f95af5025c
Katherine Wu <kwu@newrelic.com>
2016-12-03 21:07:21 -0800

    Day 3: Run input to count valid triangle sets

--- day_3.rb
+++ day_3.rb
@@ -1,3 +1,11 @@
 def valid_triangle?(a, b, c)
   a+b > c && b+c > a && a+c > b
 end
+
+# part A
+# valid_triangles_count = 0
+# File.readlines('day_3_input.txt').each do |line|
+#   lengths = line.split
+#   valid_triangles_count += 1 if valid_triangle?(lengths[0].to_i, lengths[1].to_i, lengths[2].to_i)
+# end
+# puts valid_triangles_count
