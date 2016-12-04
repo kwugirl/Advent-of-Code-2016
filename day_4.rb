db505236ab9449547463c207a75e5d2d36d967e0
Katherine Wu <kwu@newrelic.com>
2016-12-03 22:11:56 -0800

    Day 4: Group letters by count

--- day_4.rb
+++ day_4.rb
@@ -3,3 +3,11 @@ def count_letter_frequencies(str)
   str.each_char {|char| freq[char] += 1 unless char == '-' }
   freq
 end
+
+def group_letters_by_count(freq)
+  grouping = {}
+  freq.each do |letter, count|
+    grouping[count] = "#{grouping[count]}#{letter}".chars.sort.join
+  end
+  grouping
+end
--- day_4_tests.rb
+++ day_4_tests.rb
@@ -9,4 +9,11 @@ class Day4Test < Minitest::Test
 
     assert_equal expected_counts, count_letter_frequencies(str)
   end
+
+  def test_group_letters_by_count
+    freq = {'a' => 5, 'b' => 3, 'z' => 1, 'y' => 1, 'x' => 1}
+    expected_grouping = {5 => 'a', 3 => 'b', 1 => 'xyz'}
+
+    assert_equal expected_grouping, group_letters_by_count(freq)
+  end
 end
