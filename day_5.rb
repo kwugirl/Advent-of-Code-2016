ce5e1c83d9b5cefcbebc7f7ba96c59b8a25e65f4
Katherine Wu <kwu@newrelic.com>
2016-12-04 21:31:45 -0800

    Day 5: Construct password

--- day_5.rb
+++ day_5.rb
@@ -1,6 +1,22 @@
 require 'digest'
 
 def interesting_hash?(str)
-  Digest::MD5.hexdigest(str)[0..4] == "00000"
+  str[0..4] == "00000"
 end
 
+def hashed_string(str)
+  Digest::MD5.hexdigest(str)
+end
+
+def construct_password(input)
+  password = []
+  i = 0
+
+  while password.length < 8
+    hashed_string = hashed_string("#{input}#{i}")
+    password << hashed_string[5] if interesting_hash?(hashed_string)
+    i += 1
+  end
+
+  password.join
+end
--- day_5_tests.rb
+++ day_5_tests.rb
@@ -7,9 +7,13 @@ class Day5Test < Minitest::Test
     test_cases = ['abc3231929', 'abc5017308', 'abc5278568']
 
     test_cases.each do |string|
-      assert interesting_hash?(string)
+      assert interesting_hash?(hashed_string(string))
     end
 
-    assert !interesting_hash?('abc42')
+    assert !interesting_hash?(hashed_string('abc42'))
+  end
+
+  def test_construct_password
+    assert_equal '18f47a30', construct_password('abc')
   end
 end
